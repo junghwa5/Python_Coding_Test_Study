bceec5ae69a1c05847280ad36550264800ca5635
junghwa5 <50373166+junghwa5@users.noreply.github.com>
2022-09-08 09:15:41 +0900

    Update programmers_k진수에서소수개수구하기_20220908.py

--- programmers_k진수에서소수개수구하기_20220908.py
+++ programmers_k진수에서소수개수구하기_20220908.py
@@ -1,4 +1,4 @@
-def convert(num, k):
+def convert(num, k): # 10진수를 k진수로 변경
     tmp = "0123456789ABCDEF"
     q, r = divmod(num, k)
     if q == 0 :
@@ -6,7 +6,7 @@ def convert(num, k):
     else :
         return convert(q, k) + tmp[r]
 
-def primenumber(x):
+def primenumber(x): # 소수 판별
     if x == 1:
         return False
     for i in range(2, int(x**0.5) + 1):
@@ -16,9 +16,9 @@ def primenumber(x):
     
 def solution(n, k):
     answer = 0
-    k_num = convert(n, k)
-    p_list = k_num.split('0')
-    for p in p_list:
+    k_num = convert(n, k) # k진수로 변경
+    p_list = k_num.split('0') # 0을 기준으로 나누어 리스트 저장
+    for p in p_list: # 소수 개수 구함
         if p != '' and primenumber(int(p)):
             answer += 1
     

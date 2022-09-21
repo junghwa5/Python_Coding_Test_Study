210d1c9202254ab343fd37f7064f8e4bbfc09842
junghwa5 <50373166+junghwa5@users.noreply.github.com>
2022-09-21 09:10:56 +0900

    Update programmers_행렬의곱셈_20220921.py

--- programmers_행렬의곱셈_20220921.py
+++ programmers_행렬의곱셈_20220921.py
@@ -5,5 +5,5 @@ def solution(arr1, arr2):
     # a1 = np.array(arr1)
     # a2 = np.array(arr2)
     answer = np.dot(arr1, arr2).tolist() # dot()은 numpy에서 행렬 곱을 해주는 것
-																				 # tolist()를 사용하여 numpy.array()를 list로 변환 
+					# tolist()를 사용하여 numpy.array()를 list로 변환 
     return answer
